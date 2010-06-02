d3d542471f8656a502253cff6dcf1809fabaea3f
Yale Spector <yale@brandeis.edu>
2010-06-02 09:13:49 -0400

    added class01

---
+++ class01/exercise1.rb
@@ -0,0 +1,10 @@
+=begin
+  Name:         Hello Brandeis
+  Assignment:   hw1.1
+  Author:       Yale Spector
+  Requirements: Ruby
+  Usage:        "ruby exercise1.rb"
+=end
+
+# Display "Hello, Brandeis" to the user
+puts "Hello, Brandeis"
