40131e9bcd1c8f1b3ecf2baf6b1b4bb9a4324ff9
Yale Spector <yale@brandeis.edu>
2010-06-09 23:02:20 -0400

    added todo to root dir

---
+++ todo/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '01af9080653900d8d423aff4edfaa8bae2f0b978c343ed6f6b60dc23f450fd1527fc542373ce9c2f402fb49acdcc0a8d64b5c47e6ba4e69b0964d50ccf88b2af';
---
+++ todo/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_todo_session',
+  :secret      => '226cc543913b3a38a32ff52e24ffba10777e7b3b706253dabf9bbb91eb4636fe7a8f4630be2bf58d6df02d3db4bb81d4fefde7d7527a210777c932db9a2c657f'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
